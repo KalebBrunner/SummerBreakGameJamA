7080711e218a606219da101eea01e84f6d40e93b
LucasBrunner <lucasbrunner@gmail.com>
2026-08-03 22:43:35 -0400

    feature: add fruit config resource

--- src/fruit.rs
+++ src/fruit.rs
@@ -3,8 +3,15 @@ use bevy::prelude::*;
 #[derive(Debug, Component)]
 pub struct Fruit {}
 
+#[derive(Resource, Default)]
+pub struct FruitConfig {
+    fruit_limit: u32,
+}
+
 pub struct FruitPlugin;
 
 impl Plugin for FruitPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.init_resource::<FruitConfig>();
+    }
 }
