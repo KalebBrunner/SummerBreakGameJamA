950f385acd216bcc35ce1e36c170fac0bbd0c263
LucasBrunner <lucasbrunner@gmail.com>
2026-08-03 22:45:28 -0400

    feature: add fruit stats resource

--- src/fruit.rs
+++ src/fruit.rs
@@ -8,10 +8,17 @@ pub struct FruitConfig {
     fruit_limit: u32,
 }
 
+#[derive(Resource, Default)]
+pub struct FruitStats {
+    fruits_eaten: u32,
+}
+
 pub struct FruitPlugin;
 
 impl Plugin for FruitPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<FruitConfig>();
+        app //
+            .init_resource::<FruitConfig>()
+            .init_resource::<FruitStats>();
     }
 }
